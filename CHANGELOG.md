ae0241a2134c568ed0456842344ba9f232bd7475
Steve Clarke <84364906+s7clarke10@users.noreply.github.com>
2024-08-12 11:18:39 +1200

    Moving to realit variant of singer-encodings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+2.1.0  (2024-08-12)
+**Changes**
+  - Making a variant realit-singer-encodings
+  - Adding pipeline to push to PyPI
+
 2.0.13 (2023-10-07)
 -------------------
 **Changes**
--- setup.py
+++ setup.py
@@ -3,16 +3,29 @@
 from setuptools import setup, find_packages
 import subprocess
 
-setup(name="singer-encodings",
-      version='0.1.3',
+setup(name="realit-singer-encodings",
+      version='2.1.0',
       description="Singer.io encodings library",
+      python_requires=">=3.8, <3.13",
       author="Stitch",
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      url="http://singer.io",
+      keywords = ["singer", "meltano", "pipelinewise", "framework"]
+      classifiers = [
+          "License :: OSI Approved :: Apache Software License",
+          # Specify the Python versions you support here.
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+          "Programming Language :: Python :: 3.11",
+          "Programming Language :: Python :: 3.12",
+          "Development Status :: 5 - Production/Stable", "Topic :: Database"
+      ]
+      url="https://github.com/s7clarke10/realit-singer-encodings",
       install_requires=[
       ],
       extras_require={
           "dev": ["nose"]
       },
+      packages=['singer-encodings'],
+      include_package_data=True
       packages=find_packages(),
 )
